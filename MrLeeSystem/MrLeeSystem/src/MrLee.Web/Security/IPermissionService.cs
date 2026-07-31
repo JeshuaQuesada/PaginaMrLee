13a99fc4106451b118b6da789495b7c289d4aa73
BryanJC2022 <bjaen90254@ufide.ac.cr>
2026-07-30 19:12:57 -0600

    Delete MrLeeSystem/MrLeeSystem directory

--- MrLeeSystem/MrLeeSystem/src/MrLee.Web/Security/IPermissionService.cs
+++
@@ -1,7 +0,0 @@
-namespace MrLee.Web.Security;
-
-public interface IPermissionService
-{
-    Task<bool> UserHasPermissionAsync(int userId, string permissionCode);
-    Task<IReadOnlyList<string>> GetPermissionsForUserAsync(int userId);
-}
--- MrLeeSystem/MrLeeSystem/src/MrLee.Web/Security/PermissionRequirement.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Authorization;
-
-namespace MrLee.Web.Security;
-
-public sealed class PermissionRequirement : IAuthorizationRequirement
-{
-    public PermissionRequirement(string permission) => Permission = permission;
-    public string Permission { get; }
-}
